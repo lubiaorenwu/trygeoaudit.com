cd3fd6dae023931cf72a718e25491116e33d3c43
lubiaorenwu-creator <lubiaorenwu-creator@users.noreply.github.com>
2026-06-16 09:32:58 +0800

    replace adsense with monetag multitag + sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "trygeoaudit.com",
-    "zoneId": 11123743
+    "domain": "5gvci.com",
+    "zoneId": 11154297
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
